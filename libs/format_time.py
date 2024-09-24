c7766ac83c3be8aa54a94ee6e95551713d99eef5
Luke Mileski <lmileski@sandiego.edu>
2024-09-23 21:13:21 -0700

    fix date bug

--- libs/format_time.py
+++ libs/format_time.py
@@ -12,13 +12,8 @@ def format_time(dt_object: dt, local_tz: ZoneInfo) -> str:
     current_time = dt.now(local_tz)
     # finding date info
     local_time = dt_object.astimezone(local_tz)
-    # formatting date as either today or yesterday w/ some logic
-    if local_time.date() == current_time.date():
-        day_label = "Today"
-    elif local_time.date() == (current_time - timedelta(days=1)).date():
-        day_label = "Yesterday"
-    else:
-        day_label = local_time.strftime('%B %d, %Y')
+    # formatting date
+    day_label = local_time.strftime('%B %d, %Y')
 
     time_str = local_time.strftime('%I:%M %p').lstrip('0')
     full_date = f"{day_label}, {time_str}"
